c10b9bba03936f6173d967218b19464b568f3fc5
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2022-09-20 22:24:15 +0200

    add protocolbuffers

--- db/main.go
+++ db/main.go
@@ -1,5 +1,11 @@
 package main
 
-func main(){
+import "google.golang.org/grpc"
+import "github.com/MangioneAndrea/simple-social-api/db/protocolbuffers"
+import "github.com/MangioneAndrea/simple-social-api/db/controllers"
 
+func main() {
+	s := grpc.NewServer()
+
+	protocolbuffers.RegisterAuthServiceServer(s, &controllers.Auth{})
 }
--- protocolbuffers/Compile.go
+++ protocolbuffers/Compile.go
@@ -1,24 +1,63 @@
 package main
 
+// pnpm install -g ts-protoc-gen
+
 import (
 	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
+const dbDir = "../db"
+const goDir = "../go"
+const tsDir = "../ts/protocolbuffers"
+
 //protoc  Services/ProtocolBuffers/Chat/chat.proto --go_out=plugins=grpc:.
 func main() {
-	err := filepath.Walk(".",
-		func(path string, info os.FileInfo, err error) error {
+	goPath := os.Getenv("GOPATH")
+	var currentPath string
+
+	pc := make([]uintptr, 1000)
+	n := runtime.Callers(0, pc)
+	frames := runtime.CallersFrames(pc[:n])
+	for {
+		frame, more := frames.Next()
+		if !more {
+			break
+		}
+		if strings.Contains(frame.File, "Compile.go") {
+			currentPath = strings.ReplaceAll(frame.File, "Compile.go", "")
+		}
+	}
+	err := os.MkdirAll(tsDir, os.ModePerm)
+	if err != nil && !os.IsExist(err) {
+		panic(err)
+	}
+
+	err = filepath.Walk(".",
+		func(p string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
-			if strings.HasSuffix(path, ".proto") {
-				p := strings.ReplaceAll(path, "", "")
-				cmd := exec.Command("protoc", p, "--go_out=plugins=grpc:.")
+			if strings.HasSuffix(p, ".proto") {
+				fmt.Println(p)
+				props := []string{
+					currentPath + p,
+					"-I", currentPath,
+					"--go_out=plugins=grpc:" + dbDir,
+					"--go_out=plugins=grpc:" + goDir,
+					"--ts_out=" + tsDir,
+				}
+				if runtime.GOOS == "windows" {
+					props = append(props, "-IC:/proto3/include/protobuf", "-I"+path.Join(goPath, "/src"))
+				}
+				cmd := exec.Command("protoc", props...)
+
 				dir, err := os.Getwd()
 				cmd.Dir = dir
 				var stderr bytes.Buffer
@@ -33,7 +72,7 @@ func main() {
 			}
 			return nil
 		})
-		fmt.Print(err)
+	fmt.Print(err)
 	if err == nil {
 		fmt.Print("All protocol buffers compiled")
 	}
