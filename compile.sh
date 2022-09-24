66726a6405d43fd20efe942be2ee3193b9a0e421
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2022-09-24 23:37:22 +0200

    fix library default export

--- compile.sh
+++ compile.sh
@@ -11,4 +11,9 @@ do
    docker run --rm -v $(pwd):/defs namely/protoc-all -d $i -l go -o db;
    docker run --rm -v $(pwd):/defs namely/protoc-all -d $i -l typescript -o ts/src/protocolbuffers;
 done
+# Fix the imports for the typescript files. The library exports the default, but the
+# generated code expects the * export
+for i in ts/src/protocolbuffers/*; do
+    sed -i -- 's/* as _m0/{ default as _m0 }/g' $i
+done
 # Compile the protocol buffers.
