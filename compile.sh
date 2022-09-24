e2f92af2394995384dd0e40e13afe4f12155d180
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2022-09-24 11:42:58 +0200

    ts add rpc for posts

--- compile.sh
+++ compile.sh
@@ -1,17 +1,14 @@
-# /bin/bash
+#! /bin/bash
 
 # This script compiles the protocol buffers for the project.
 if [[ "$(docker images -q namely/protoc-all 2> /dev/null)" == "" ]]; then
     docker pull namely/protoc-all;
 fi
-
 allProtoFolders=($(ls -d protocolbuffers/*))
-
 for i in "${allProtoFolders[@]}"
 do
    docker run --rm -v $(pwd):/defs namely/protoc-all -d $i -l go -o go/$i;
    docker run --rm -v $(pwd):/defs namely/protoc-all -d $i -l go -o db/$i;
    docker run --rm -v $(pwd):/defs namely/protoc-all -d $i -l typescript -o ts/src/protocolbuffers;
 done
-
 # Compile the protocol buffers.
--- ts/src/services/post/index.ts
+++ ts/src/services/post/index.ts
@@ -1,10 +1,45 @@
-import { createPost, getPosts } from "../../protocolbuffers/posts";
-import * as Util from "../../util";
+import grpc from "@grpc/grpc-js";
+import { Message, Method, rpc, RPCImpl, RPCImplCallback } from "protobufjs/minimal";
+import {
+  Pagination,
+  Post,
+  PostsServiceClientImpl,
+} from "../../protocolbuffers/posts";
 
-export const create = (title: string, content: string) => {
-  return createPost(title, content);
+const grpcServerUrl = "0.0.0.0:50051";
+
+const Client =  grpc.makeGenericClientConstructor({}, "PostsService", {});
+const client = new Client(grpcServerUrl, grpc.credentials.createInsecure());
+
+const rpcImpl: RPCImpl = (
+  method: Method, // | rpc.ServiceMethod<Message<{}>, Message<{}>>,
+  requestData: Uint8Array,
+  callback: RPCImplCallback
+) => {
+  client.makeUnaryRequest(
+    method.service.name + "/" + method.name,
+    (message: Message<{}>) => message.toArrayBuffer(),
+    (message: Uint8Array) => Post.decode(message),
+    requestData,
+    (err: Error | undefined, response: Uint8Array) => {
+      if (err) {
+        callback(err, null);
+      } else {
+        callback(null, response);
+      }
+    }
+  );
 };
 
-export const get = (skip: number, limit: number) => {
-  return getPosts(skip, limit);
+export const PostService = new PostsServiceClientImpl(rpcImpl);
+
+export const create = (title: string, content: string) => {
+  return PostService.CreatePost(Post.fromJSON({ title, content }));
+};
+
+export const get = async (skip: number, limit: number) => {
+  const { posts } = await PostService.GetPosts(
+    Pagination.fromJSON({ skip, limit })
+  );
+  return posts;
 };
